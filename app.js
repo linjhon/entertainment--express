236db33dbb6209a5a7b6a30fc33e60acf317a4c0
林强 <461276106@qq.com>
2017-04-22 15:46:58 +0800

    数据库连接

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+
+
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 var book = require('./routes/book');
