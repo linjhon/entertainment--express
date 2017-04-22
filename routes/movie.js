80488d43464af607971df8142b136f95cacaff3d
Jafish <526800921@qq.com>
2017-04-22 17:56:49 +0800

    no message

--- routes/movie.js
+++ routes/movie.js
@@ -7,28 +7,6 @@ var DB_CONN_STR = 'mongodb://10.31.155.62:27017/happy'; // 设置连接字符串
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	var selectData = function(db, callback) {
-	  //连接到表
-	  var collection = db.collection('happy');
-	  //查询数据
-	  var whereStr = {};
-	  collection.find(whereStr).toArray(function(err, result) {
-	    if(err)
-	    {
-	      console.log('Error:'+ err);
-	      return;
-	    }
-	    callback(result);
-	  });
-	}
-
-	MongoClient.connect(DB_CONN_STR, function(err, db) {
-	  console.log("连接成功！");
-	  selectData(db, function(result) {
-	    console.log(result);
-	    db.close();
-	  });
-	});
     res.render('movie', { title: "电影列表" });
 });
 
