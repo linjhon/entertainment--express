1f00d1eeb9fee1930ab972c3de7ec2988d8558c2
wxl1113 <1369270324@qq.com>
2017-04-22 16:15:25 +0800

    no message

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 
 
-
+var session = require('express-session');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var book = require('./routes/book');
@@ -27,7 +27,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({
+  secret:'recommend 128 bytes random string', 
+  cookie:{maxAge:20*60*1000}, // 20分钟 ,毫秒为单位
+  resave:true,  // 如果来了一个新的请求，不管原来存在不存在，重新存储一个
+  saveUninitialized:true // 存储一些未初始化的session内容
+}))
 app.use('/', index);
 app.use('/users', users);
 app.use('/book', book);
