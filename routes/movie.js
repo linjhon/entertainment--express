2d9e6b85f1f11f86e148c0115e766e01afa3411f
yujunmoxi <1938832242@qq.com>
2017-04-24 22:26:35 +0800

    电影评论

--- routes/movie.js
+++ routes/movie.js
@@ -105,7 +105,7 @@ router.get('/moviedetail', function(req, res, next) {
        }
      });
 
-<<<<<<< HEAD
+
         //查找电影评论信息;
      
       var bookdetaildata={
@@ -131,8 +131,7 @@ router.get('/moviedetail', function(req, res, next) {
          db.close();
        }
      });
-=======
->>>>>>> 901b3d2b191936a13ca00a9e7901f4ab629604c5
+
 
     }
   })
