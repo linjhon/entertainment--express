567d8eac51ee3e7edce4d3f81631d6a3d6e9a875
Jafish <526800921@qq.com>
2017-04-24 13:53:25 +0800

    no message

--- routes/movie.js
+++ routes/movie.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var DB_CONN_STR = 'mongodb://10.31.155.62:27017/happy';
-var async=require('async')
+var async=require('async');
+
+//跨域
+/*var http = require("http");
+var cheerio = require("cheerio");
+var fs =require('fs');*/
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -69,6 +74,56 @@ router.get('/', function(req, res, next) {
     }
   })
   //title值，请勿修改，头部组件判断使用;
+  
+
+
+
+  
+
+// Utility function that downloads a URL and invokes
+// callback with the data.
+/*function download(url, callback) {
+  http.get(url, function(res) {
+    var data = "";
+    res.on('data', function (chunk) {
+      data += chunk;
+    });
+    res.on("end", function() {
+      callback(data);
+    });
+  }).on("error", function() {
+    callback(null);
+  });
+}
+
+
+
+    var url = "https://movie.douban.com/j/subject_suggest?q=速度";
+    download(url, function(data) {
+    	console.log(data);
+      if (data) {
+          //console.log(data);
+          var $ = cheerio.load(data);
+          var total =$(".fred").text();
+          fs.appendFile('./number.txt',total,'utf-8',function(err){
+              if(err) {throw err;}
+          });
+          var numArrs=$("div[class='d d-num']").text();
+          numArrs =numArrs.replace(/,/g,'');//把所有的逗号变为空(数字中间的逗号)
+          numArrs =numArrs.replace(/\s+/g, ',');//把所有的空字符串变为一个逗号(数字与数字之间变为逗号)
+          numArrs =numArrs.replace(/,$/gi, '');//去除最后一个逗号
+          fs.appendFile('./number.txt',numArrs,'utf-8',function(err){            
+              if(err) {throw err;}
+          });
+          console.log("done");
+        }
+        else console.log("error");  
+    });*/
+
+
+
+
+
 });
 
 router.get('/moviedetail', function(req, res, next) {
